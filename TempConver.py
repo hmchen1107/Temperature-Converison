b812330d325a467d07b93b904e4d60f6efe02359
hmchen1107 <langing8420@gmail.com>
2021-01-24 20:53:21 +0800

    add new file

---
+++ TempConver.py
@@ -0,0 +1,12 @@
+#Fahrenheit to Celsius formula
+# F = C*(9/5)+32
+#Celsisu to Fahrenheit formula
+
+#TEST : Let user input Celsius then print Fahrenheit
+
+tem_c = input ('PLS input the Celsius temperature : ')
+
+tem_f = float(tem_c)*(9/5)+32
+
+print("Fahrenheit is",tem_f)
+
